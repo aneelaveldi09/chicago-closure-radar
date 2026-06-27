da3b2cfbd1d5d7f7b3d217ddb308ea3301a2da5c
Aneela Veldi <aneelaveldi@Aneelas-MacBook-Air-2.local>
2026-06-26 22:30:27 -0500

    Remove smooth cursor (too laggy)

--- web/src/components/client-shell.tsx
+++ web/src/components/client-shell.tsx
@@ -1,17 +1,5 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const SmoothCursor = dynamic(
-  () => import("@/components/ui/smooth-cursor").then((m) => m.SmoothCursor),
-  { ssr: false }
-);
-
 export function ClientShell({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <SmoothCursor />
-    </>
-  );
+  return <>{children}</>;
 }
